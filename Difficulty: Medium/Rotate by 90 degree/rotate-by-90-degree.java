5a7890b8042e08d3669a52c273ebb71f8e82adaa
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2024-11-05 16:17:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] arr = new int[n][n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) arr[i][j] = sc.nextInt();
+
+            GFG g = new GFG();
+            g.rotate(arr);
+            printMatrix(arr);
+
+            System.out.println("~");
+        }
+    }
+
+    static void printMatrix(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) System.out.print(arr[i][j] + " ");
+            System.out.println("");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// User function Template for Java
+
+class GFG {
+    static void rotate(int mat[][]) {
+        // Code Here
+               int n = mat.length;
+
+        // Initialize the result matrix with zeros
+        int[][] res = new int[n][n];
+
+        // Flip the matrix clockwise using nested loops
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][n - i - 1] = mat[i][j];
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(res[i], 0, mat[i], 0, n);
+        }
+    }
+}
