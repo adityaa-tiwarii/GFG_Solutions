5c467be9f287836c5f3464129d2a783d0232de8c
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2024-11-24 18:51:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+// User function Template for Java
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int[] arr) {
+        int cSum=0,tSum=Integer.MIN_VALUE,left=0;
+        for(int i=0;i<arr.length;i++){
+            cSum+=arr[i];
+            tSum=Math.max(tSum,cSum);
+            if(cSum<0){
+                cSum=0;
+            }
+        }
+        return tSum;
+    }
+}
