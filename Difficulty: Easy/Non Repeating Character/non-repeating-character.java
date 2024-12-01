73db74a309839cdae0d753c6198ae28ccc7ac1a3
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2024-12-01 16:53:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String st = sc.next();
+
+            char ans = new Solution().nonRepeatingChar(st);
+
+            if (ans != '$')
+                System.out.println(ans);
+            else
+                System.out.println(-1);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+
+class Solution {
+    static char nonRepeatingChar(String s) {
+        Map<Character, Integer> frequencyMap = new LinkedHashMap<>();
+        
+        // Count the frequency of each character in the string
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Find the first character with a frequency of 1
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey(); // Return the first non-repeating character
+            }
+        }
+
+        // If no non-repeating character is found, return '$'
+        return '$';
+    }
+}
+
+
+
+
+
