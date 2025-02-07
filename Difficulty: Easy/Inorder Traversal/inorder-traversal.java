ae9259c66abe624a06d12c3c70605e45d8a2279b
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2025-02-07 23:34:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.java
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            ArrayList<Integer> res = g.inOrder(root);
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.print("\n");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+/* A Binary Tree node
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+
+
+/* A Binary Tree node
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    //Step 1: Function to start Inorder Traversal
+    ArrayList<Integer> inOrder(Node root) {
+        // Code
+        ArrayList<Integer> res = new ArrayList<Integer>(); //Create a list to store the result
+        inorderUtils(root, res);   //Call helper function
+        return res;  //Return the final list
+    }
+    
+    //Step 2-5: Helper function for Inorder Traversal (Recursive)
+    void inorderUtils(Node root, ArrayList<Integer> res)
+    {
+        if(root == null)  //Step 2: If node is 'null', stop recursion
+        {
+            return;
+        }
+        
+        inorderUtils(root.left, res); //step 3: Visit 'Left' Subtree
+        res.add(root.data);           //step 4: Store current Node
+        inorderUtils(root.right, res);//step 5: Visit 'Right' Subtree
+    }
+}
