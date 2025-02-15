366086e611af13129ce01dd85fa44df97daba407
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2025-02-15 23:56:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.java
+++ Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.java
@@ -118,6 +118,20 @@ class GfG {
 // } Driver Code Ends
 
 
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
 
 class Solution {
     Node LCA(Node root, Node n1, Node n2) {
