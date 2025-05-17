f6afd525b016f746dce9e17003037efdecb4f41c
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2025-05-17 22:47:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort the given array after applying the given equation/sort-the-given-array-after-applying-the-given-equation.cpp
+++ Difficulty: Medium/Sort the given array after applying the given equation/sort-the-given-array-after-applying-the-given-equation.cpp
@@ -7,17 +7,12 @@ using namespace std;
 
 class Solution {
   public:
-   vector<int> sortArray(vector<int> &arr, int A, int B, int C) {
+    vector<int> sortArray(vector<int> &arr, int A, int B, int C) {
         // code here
-        int n=arr.size();
-        for(int i=0; i<n; i++){
-            int x=arr[i];
-            arr[i]=(A*x*x+B*x+C);
-        }
-        sort(arr.begin(), arr.end());
-        return arr;
+        
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
