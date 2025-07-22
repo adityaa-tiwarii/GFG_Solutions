55c040605ba35b439415a0f710fd9293e42ef7b9
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2025-07-22 23:22:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -1,20 +1,28 @@
 class Solution {
     public int missingNumber(int[] arr) {
         // code here
-        Set<Integer> set=new LinkedHashSet<>();
         Arrays.sort(arr);
-        int sum=0;
-        for(int i:arr){
-            if(i>0){
-                set.add(i);
+        int pos1=-1, n=arr.length, prev=1;
+        for(int x=0; x<n; x++) //we want the position of element 1
+        {
+            if(arr[x]==1)
+            {
+                pos1=x; //store the position of element 1
             }
+            if(arr[x]>1) //since its sorted if any element is greater than 1 we know 1 cannot be found after it. 
+                break;
         }
-        int j=1;
-        for(int i:set){
-            if(j!=i) break;
-            j++;
-        }
-        return j;
+        if(pos1==-1) // 1 is not present
+            return 1;
+        if(pos1==n-1) // eg: -1 0 1 1 is the last element of sorted array
+            return 2;
         
+        for(int x=pos1+1; x<n;x++)
+        {
+            if(prev+1!=arr[x] && prev!=arr[x]) // checks for repeated elemnt and if the prev element +1 != current element
+                return prev+1;
+            prev=arr[x];
+        }
+        return prev+1;
     }
 }
