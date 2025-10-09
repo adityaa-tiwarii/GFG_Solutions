91a0caa3bb41414cca0014df2ec2b8c4b2d619d2
adityaa-tiwarii <iamadityatiwari32@gmail.com>
2025-10-09 22:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    ArrayList<Integer> list=new ArrayList<>();
+    public ArrayList<Integer> postOrder(Node root) {
+        // code here
+        if(root == null) return list;
+        postOrder(root.left);
+        postOrder(root.right);
+        list.add(root.data);
+        
+        return list;
+        
+    }
+}
